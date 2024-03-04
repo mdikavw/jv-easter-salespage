9261163295744b82222f95929484ecc2213c7a94
mdikavw <mdikavw@gmail.com>
2024-03-04 08:19:07 +0700

    [FIX] Change bootstrap version to downloadable bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/custom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
@@ -53,7 +53,7 @@
     <div id="purchase"></div>
     <?php include './includes/purchase.php' ?>
     <?php include './includes/faq.php' ?>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
