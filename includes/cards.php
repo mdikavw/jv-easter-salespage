c095334c12e1076a6fdb0494f96edc8fa3622691
mdikavw <mdikavw@gmail.com>
2024-03-04 09:39:22 +0700

    [FEAT] Add carousel

--- includes/cards.php
+++ includes/cards.php
@@ -1,44 +1,19 @@
-<div class="container-fluid my-5">
-    <div class="d-flex flex-wrap align-items-center justify-content-center">
-        <div class="card m-2">
-            <img src="https://via.placeholder.com/400x150" class="card-img" alt="Placeholder Image" />
-            <div class="card-img-overlay">
-                <h5 class="card-title text-center">
-                    25 printable SVGs for your product
-                </h5>
-            </div>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="asset/carousel/1.png" class="d-block w-50 m-auto" alt="..." style="max-width: none">
         </div>
-        <div class="card m-2">
-            <img src="https://via.placeholder.com/400x150" class="card-img" alt="Placeholder Image" />
-            <div class="card-img-overlay">
-                <h5 class="card-title text-center">
-                    Adult's and Child's Coloring Books
-                </h5>
-            </div>
+        <div class="carousel-item">
+            <img src="asset/carousel/2.png" class="d-block w-50 m-auto" alt="..." style="max-width: none">
         </div>
-        <div class="card m-2">
-            <img src="https://via.placeholder.com/400x150" class="card-img" alt="Placeholder Image" />
-            <div class="card-img-overlay">
-                <h5 class="card-title text-center">
-                    25 Quotes Post Idea for Social Media
-                </h5>
-            </div>
+        <div class="carousel-item">
+            <img src="asset/carousel/3.png" class="d-block w-50 m-auto" alt="..." style="max-width: none">
         </div>
-        <div class="card m-2">
-            <img src="https://via.placeholder.com/400x150" class="card-img" alt="Placeholder Image" />
-            <div class="card-img-overlay">
-                <h5 class="card-title text-center">
-                    25 Pinterest and Story Contents
-                </h5>
-            </div>
+        <div class="carousel-item">
+            <img src="asset/carousel/4.png" class="d-block w-50 m-auto" alt="..." style="max-width: none">
         </div>
-        <div class="card m-2">
-            <img src="https://via.placeholder.com/400x150" class="card-img" alt="Placeholder Image" />
-            <div class="card-img-overlay">
-                <h5 class="card-title text-center">
-                    25 Viral Videos for your Post
-                </h5>
-            </div>
+        <div class="carousel-item">
+            <img src="asset/carousel/5.png" class="d-block w-50 m-auto" alt="..." style="max-width: none">
         </div>
     </div>
 </div>
